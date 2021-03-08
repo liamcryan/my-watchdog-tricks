3099d6ab6923e2a580d87c26dd9b693c450d1ab0
Gogs <gogs@fake.local>
2021-03-07 21:10:14 -0800

    pysimplegui system tray execute watchmedo in subprocess

---
+++ thoughts.rst
@@ -0,0 +1,9 @@
+some thoughts
+-------------
+
+Assume this gets run by each teammember via pysimplegui (system tray).
+
+What if the team member uses an old version that doesn't work right?
+Their copy will be doing wrong things, right?
+
+How to avoid this?
---
+++ watchme.py
@@ -0,0 +1,27 @@
+import subprocess
+
+import PySimpleGUIQt as sg
+
+TRICKS_FILE = '/Users/liamcryan/PycharmProjects/watchlog/tricks.yaml'
+
+menu_def = ['BLANK', ['Restart', '---', 'Exit']]
+tray = sg.SystemTray(menu=menu_def,
+                     filename=r'icon.png',
+                     )
+
+
+def watch_process() -> subprocess.Popen:
+    return subprocess.Popen(['watchmedo', 'tricks-from', 'tricks.yaml'], shell=False)
+
+
+process = watch_process()
+
+while True:
+    menu_item = tray.read()
+
+    if menu_item == 'Exit':
+        break
+    if menu_item == 'Restart':
+        process.kill()
+        process.wait()
+        process = watch_process()
